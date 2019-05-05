7b1f88811766bcf3a40672545aac18732ee548a7
boolean <13352840347@163.com>
2019-05-05 17:08:06 +0800

    primaryCorlor 使用设计稿的颜色

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
      theme: new ThemeData(
-        primaryColor: Colors.brown,
-        primarySwatch: Colors.brown,
+        primaryColor: Color.fromRGBO(81, 81, 89, 1),
+        primarySwatch: Colors.brown,//primarySwatch只有几大类型，但是可以通过其他参数覆盖它
         scaffoldBackgroundColor :Colors.black87
       ),
       home: MainPageWidget(),//启动MainPage
